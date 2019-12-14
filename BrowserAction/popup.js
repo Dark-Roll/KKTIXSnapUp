f5c5afcdf488aacdd51f2e0264591d08f0d4b59b
WillyBonny <willy.yang@bonny-live.com>
2019-12-15 02:12:42 +0800

    Complete a successful version

--- BrowserAction/popup.js
+++ BrowserAction/popup.js
@@ -1,5 +1,10 @@
 //popup.js  
 
+
+// alert 出來 重新整理
+// a ticket but buy 4 error handling
+
+
 // keep the default in HTML ???
 // 換頁 和重新整理就消詩了
 // 原先的 [0].click() 要清掉
@@ -7,100 +12,142 @@
 // when popup.html is loaded, as the meaning of the extension
 // should be clicked
 
-// NodeList is not an dom (?), so it will always set [element] to it
+
+{/* <all_urls> */ }
+
+// close 變數
+
+// 感覺可以喔，不行啦 關掉也會重啟
+// 折衷的方法就是打開不要收起來XDDD
+// 剩下到了特定網址傳了
+// then we can just when the url is matched, send to content
+
+// send twice 
+// total fifth ( wtf?)
+// 應該是 三次 前面那兩次是 refresh 的樣子
+let ticketType = 0
+let ticketNumber = 0
+let currentUrl 
+
+
+let ticketTypeSelect = document.getElementsByName('ticketType')[0]
+let ticketNumberSelect = document.getElementsByName('ticketNumber')[0]
+
+ticketNumberSelect.addEventListener('change', function () {
+    ticketNumber = ticketNumberSelect.options[ticketNumberSelect.selectedIndex].text;
+    ticketNumber = parseInt(ticketNumber, 10)
+})
+
+ticketTypeSelect.addEventListener('change', function () {
+    ticketType = ticketTypeSelect.options[ticketTypeSelect.selectedIndex].text;
+    ticketType = parseInt(ticketType, 10)
+})
+
+
+// 其實 url 可以直接加在這邊...
+// like this
+// chrome.tabs.query({
+//     active: true,
+//     currentWindow: true
+// }, function (tabs) {
+//     var tab = tabs[0];
+//     var url = tab.url;
+// });
+const sendToContentScript = (ticketUpdateObj) =>{
+    const doInCurrentTab = ()=> {
+        // tabArray .id 會根據視窗做 id 的變動
+        chrome.tabs.query(
+            { currentWindow: true, active: true},
+            function(tabArray) { 
+                console.log(tabArray[0]) 
+                chrome.tabs.sendMessage(
+                    tabArray[0].id,
+                    ticketUpdateObj
+                )
+            }
+        );
+    }
+    doInCurrentTab()
+}
+
+
+// 到對的 url 才傳到 contentScript
+const getUpdateUrl = ()=>{
+    chrome.tabs.onUpdated.addListener(function(tab){
+        console.log("tab in eventListener is :", tab);
+        chrome.tabs.query( {active: true}, function(tabQueryResult){
+            let updateTabElement = tabQueryResult.filter(e=>{
+                return tab === e.id
+            })
+            currentUrl = updateTabElement[0] && updateTabElement[0].url
+
+
+            // 符合網址才傳
+            if( currentUrl && currentUrl.indexOf('kktix') >-1){
+                console.log("come on, let's send to contentScript");
+                sendToContentScript({ticketType, ticketNumber})
+            }
+        })
+    });
+} 
+getUpdateUrl()
+
+
+
+
+
+
+
+
+
 
 
 //  ?????????????? why local can change the global variable
-document.addEventListener('DOMContentLoaded', function(dcle) {  
-    
-    // it will work, when [0] added after getDom
-    let ticketTypeSelect = document.getElementsByName('ticketType')[0]
-    let ticketNumberSelect = document.getElementsByName('ticketNumber')[0]
-    
-    // default value
-    let ticketType = 1
-    let ticketNumber = 1
+// document.addEventListener('DOMContentLoaded', function(dcle) {  
 
-    let submitButton = document.getElementsByClassName('submit')[0]
+//     // it will work, when [0] added after getDom
+//     let ticketTypeSelect = document.getElementsByName('ticketType')[0]
+//     let ticketNumberSelect = document.getElementsByName('ticketNumber')[0]
 
-    console.log("is loaded");
+//     // default value
+//     let ticketType = 1
+//     let ticketNumber = 1
 
+//     let submitButton = document.getElementsByClassName('submit')[0]
 
-    ticketNumberSelect.addEventListener('change', function(){
+//     console.log("is loaded");
 
-        console.log(ticketNumberSelect.selectedIndex );
-        ticketNumber = ticketNumberSelect.options[ticketNumberSelect.selectedIndex].text;
-        ticketNumber =  parseInt(ticketNumber, 10)
-        console.log(ticketNumber);
-    
-    
-        // sendToContentScript("ticketNumber", ticketNumber)
-
-    
-    })
+//     function saveOptions() {
 
 
-    ticketTypeSelect.addEventListener('change', function(){
-
-        console.log(ticketTypeSelect.selectedIndex );
-        ticketType = ticketTypeSelect.options[ticketTypeSelect.selectedIndex].text;
-        ticketType=  parseInt(ticketType, 10)
-        console.log(ticketType);
-    
-    
-        // sendToContentScript("ticketType", ticketType)
-    
-    })
-
-    // var currSelectText = document.all.objSelect.options[document.all.objSelect.selectedIndex].text;
-
-    submitButton.addEventListener('click', function(){
-        console.log(ticketType, ticketNumber);
-        sendToContentScript({
-            "ticketType": ticketType-1,
-            "ticketNumber" : ticketNumber,
-        })
-    })
-
-    // const sendToContentScript = (element, value ) =>{
-    const sendToContentScript = (ticketUpdateObj) =>{
-
-        function doInCurrentTab(){
-            let a 
-            // tabArray .id 會根據視窗做 id 的變動
-            chrome.tabs.query(
-                { currentWindow: true, active: true},
-                function(tabArray) { 
-                    console.log(tabArray[0]) 
-                    chrome.tabs.sendMessage(
-                        tabArray[0].id,
-                        // {
-                        //     "ticketNumber": ticketNumber,
-                        //     "ticketType": ticketType
-                        // }
-                        // why [element] work ??
-                        ticketUpdateObj
-                    )
-    
-                    // return tabArray[0]
-                }
-    
-            );
-            
-        }
-        let activeTabId 
-        doInCurrentTab( function(tab){
-            console.log(tab);
-            activeTabId = tab.id
-        } )
-            // doInCurrentTab()
-        
-        console.log(activeTabId);
-    }
+//         ticketNumber = ticketNumberSelect.options[ticketNumberSelect.selectedIndex].text;
+//         ticketNumber =  parseInt(ticketNumber, 10)
+//         localStorage["favColor"] = color;
+//     }
 
 
-    // ticketNumberSelect.addEventListener('click', function(ce) {  
-    //     //使用Chrome API開啟視窗  
-    //     chrome.windows.create({ "url": "https://github.com/lauraluo" });  
-    // });  
-});
+
+
+//     submitButton.addEventListener('click', function(){
+//         console.log(ticketType, ticketNumber);
+
+//         // save
+//         // chrome.storage.sync.set({
+//         //     "ticketType": ticketType-1,
+//         //     "ticketNumber" : ticketNumber,
+//         // }, function(){
+//         //     console.log("success storage");
+//         // })
+
+
+
+//     })
+
+
+
+
+//     // ticketNumberSelect.addEventListener('click', function(ce) {  
+//     //     //使用Chrome API開啟視窗  
+//     //     chrome.windows.create({ "url": "https://github.com/lauraluo" });  
+//     // });  
+// });
--- buyTicket.js
+++ buyTicket.js
@@ -1,21 +1,137 @@
 
-			// "matches" : ["https://kktix.com/events/*/registrations/new"],
+// "matches" : ["https://kktix.com/events/*/registrations/new"],
 // {/* <all_urls>  </all_urls> */} for test
 
-function buyTicket (ticketType, ticketNumber) {
 
-    console.log("ready to if")
-    let Tickets = document.querySelectorAll('.btn-default.plus')
-    if (Tickets.length < 1) return setTimeout(function () { buyTicket() }, 1300)
-    console.log("get Element")
+
+
+// why sold out?
+// what is DOMTokenList
+// two bugs:
+// 1. unknown sold out (can't load the newest)
+// 2. unknown send third 
+// 3. default 跑第一張 (怎麼樣不要 default), default 跑一次勾 onMessage 再跑一次就沒了
+
+// refresh when get alert 
+// refresh when get noTicketAlert
+    // getComputedStyle
+
+
+//   function getStyle(el, styleProp) {
+        // defaultView.getComputedStyle(nextStepButton, null).getPropertyValue(styleProp);
+
+        // let sty =window.getComputedStyle(nextStepButton, null).cssText
+        // console.log("get DOM style uses above function, ", sty);
+/*       
+    bonus:
+    choose ticket type and number 
+*/
+
+
+
+// submit and again answer may error
+// input answer
+// // submit click (it will click now)
+// alert 要拿掉嗎
+// alert wont reload, catch style to deal with? because the two is the same
+
+
+let answer ="aiba_snake@yahoo.com.hk"
+// let answer ="0525jasonmra"
+// let answer ="0309cat" 
+// let answer ="jjc" 
+// alert 沒有取消
+
+// console.log(answer);
+// let alert wont show
+window.alert = function() {};
+
+
+// no ticket warning happens, refresh
+// and autoFill right answer
+
+// 點了別的頁面會重新整理就會全部清掉
+// 所以資料只能存 popup.js
+ 
+// setTimeout(
+// ()=>{if(alert('請選擇專案類型')){console.log('a')}}
+// , 1000)
+
+
+
+// window.onload = () => {
+//     if (window.location.href.match(/https:\/\/kktix\.com\/events\/[\w-+&@#/%?=~|!:,.;]+\/registrations\/new/)) {
+//         console.log("match");
+//         buyTicket(ticketType, ticketNumber)
+//     } else {
+//         console.log("no");
+//     }
+// }
+
+const buyTicket = (ticketType, ticketNumber) => {
+// function buyTicket() {
+
+    console.log("in bg", chrome);
+
+    let noTicketAlert = document.querySelectorAll('.register-status.register-status-OUT_OF_STOCK .alert-wrapper.alert-warning.align-center .alert')
+    // noTicketAlert.length
+    // console.log(noTicketAlert.length);
+
+    if(noTicketAlert.length>1){
+        console.log("no ticket alert");
+        location.reload() 
+    }
+
+    // register-status register-status-IN_STOCK register-status-SOLD_OUT register-status-COMING_SOON
+    // hide register-status register-status-IN_STOCK register-status-SOLD_OUT register-status-COMING_SOON
     
+    let soldOutTicketPage = document.querySelectorAll('.register-status.register-status-IN_STOCK.register-status-SOLD_OUT.register-status-COMING_SOON')[0]
+    console.log('soldOutTicketPage is :', soldOutTicketPage);
+    if ( soldOutTicketPage && soldOutTicketPage.classList.contains('hide') === true ){
+        console.log( soldOutTicketPage.classList)
+        console.log("sold out");
+        // location.reload() 
+    }
+
+
+    // Class =  register-status register-status-OUT_OF_STOCK
+	// class = alert-wrapper alert-warning align-center
+ 	// 	class = alert
+
+    // if(!ticketType) ticketType = 0
+    // if(!ticketNumber) ticketNumber = 1
+    // console.log( chrome.extension.getBackgroundPage().window.ticketType )
+    console.log("ready to if")
+    // console.log("ticketType and ticketNumber in buyTicket", ticketType, ticketNumber);
+
+    // console.log(ticType, ticNumber);
+    let Tickets = document.querySelectorAll('.btn-default.plus')
+    if (Tickets.length < 1) return setTimeout(function () { buyTicket() }, 10)
+    console.log("get Element")
+
+
     //  幾張
     // 哪一張
-    for (let i = 0; i < ticketNumber; i++) {
-        
-        Tickets[ticketType].click()
+    // for (let i = 0; i < ticketNumber; i++) {
+
+    //     Tickets[ticketType].click()
+    // }
+    // if(ticketType || ticketType === 0 && Tickets[ticketType-1] ){ // undefined 可以進來？
+    if( typeof(ticketType) === 'number' && Tickets[ticketType-1] ){
+        // ticketType 是一個鬼東東 event
+        console.log('ticketType is :', ticketType);
+        Tickets[ticketType-1].click()
+
+    }else{
+        console.log("no ticketType");
+        Tickets[0].click()
+        Tickets[0].click()
+        if(Tickets[0]){
+            // Tickets[0].click()
+            // Tickets[0].click()
+    
+        }
     }
-    Tickets[0].click()
 
     // col-6 form-control ng-valid ng-touched ng-not-empty ng-pristine 
     // col-6 form-control ng-valid ng-touched ng-dirty ng-valid-parse ng-empty
@@ -26,6 +142,19 @@ function buyTicket (ticketType, ticketNumber) {
     let answerInput = document.querySelector('.col-6.form-control.ng-valid')
     if (answerInput) answerInput.focus()
     // IF NO click 下一步?
+    
+    if(answer.length> 0 && answerInput){
+        
+        answerInput.value = answer
+        var event = new Event('change');
+        // 監聽事件
+        // elem.addEventListener('build', function (e) { ... }, false);
+        // 觸發事件
+        answerInput.dispatchEvent(event);
+    }
+
+     
+    
     // answerInput.value = "C"
 
     // focuses on a form field element even if it has tabIndex
@@ -33,55 +162,87 @@ function buyTicket (ticketType, ticketNumber) {
 
 
     let checkAgreeInput = document.querySelector('#person_agree_terms')
+    // because click has monitored eventListener
     checkAgreeInput.click()
     // checkAgreeInput.checked=true
 
     // 選位 這鳩真的看手塑了
     // 完成選位
 
+
+    // await
+
     // 和 Enter 作連動
     let nextStepButton = document.querySelector('.btn.btn-primary.btn-lg.ng-isolate-scope')
+    if(nextStepButton){
+        // nextStepButton.click()
+
+        const fillAnswerAgain = ()=>{
+            console.log(answerInput);
+            if (answerInput.disabled == true ){
+                return setTimeout(() => { fillAnswerAgain() }, 10)
+            }
+            answerInput.focus()
+        }
+
+        let wrongAnswerAlert = document.querySelectorAll('.custom-captcha-inner .ng-binding')
+        if(wrongAnswerAlert[0]) fillAnswerAgain()
+    }
     // if(nextStepButton) nextStepButton.click()
     document.onkeydown = function (e) {  //對整個頁面文件監聽 
         if (e.keyCode == 13) {
             nextStepButton.click()
+
+            const fillAnswerAgain = ()=>{
+                console.log(answerInput);
+                if (answerInput.disabled == true ){
+                    return setTimeout(() => { fillAnswerAgain() }, 10)
+                }
+                answerInput.focus()
+            }
+
+            let wrongAnswerAlert = document.querySelectorAll('.custom-captcha.ng-scope.error') // custom-captcha ng-scope error
+            // 不一定馬上抓得到
+            console.log(wrongAnswerAlert);
+            // .custom-captcha-inner
+            // .ng-binding
+
+            // let wrongAnswerAlert = document.querySelectorAll('.custom-captcha-inner .ng-binding')
+            // console.log(wrongAnswerAlert, wrongAnswerAlert[0]);
+
+            // if(wrongAnswerAlert[0]) 
+            fillAnswerAgain()
+            
+
         }
     };
 }
 
-// chrome.webNavigation.onCompleted.addListener(buyTicket)
 // document.addEventListener('DOMContentLoaded', function(){
 //     buyTicket()
 // });
+
+
+
 window.onload = buyTicket
 
-function updateBuyTicket(json){
-
-    console.log(json);
-    buyTicket(json.ticketType, json.ticketNumber)
-    // let 
-    // if(!json) return
-    if (json.ticketType){
-        buyTicket("",)
-    }
-    if(json.ticketNumber){
-
-    }
-    // Object.keys(json).map( e =>{
-
-    //     buyTicket()
-    // })
+const handleBuyTicket = (ticketObj) => {
+    console.log(ticketObj);
+    buyTicket(ticketObj.ticketType, ticketObj.ticketNumber)
 }
 
-function print(message){
-    console.log(message);
-}
+chrome.runtime.onMessage.addListener(handleBuyTicket)
 
-// browser.
-// chrome.runtime.onMessage(print)
-// browser.
-chrome.runtime.onMessage.addListener(updateBuyTicket)
-console.log("in content script");
+
+
+//     // if (chrome.storage && chrome.storage.sync){
+//     //     console.log( chrome.storage.sync.get(null));
+
+//     // } else {
+//     //     console.log("no chrome storage");
+// }
+
+
 
 
 
